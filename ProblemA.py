6b04c9eb7b1842f71ac67eb530111ceb6caea13d
chrisjmorgan <chrisjamesmorgn@gmail.com>
2020-01-27 22:05:34 -0800

    First commit

--- ProblemA.py
+++ ProblemA.py
@@ -1,4 +1,65 @@
-def my_function():
-  print("Hello from a function")
+def tda(imgFile,nr,nc,thresholds):
+    try:
+        with open(imgFile) as f:
+            imgstr = f.read()
+    except:
+        print("File open error")
+    imgStr2Int = [int (i) for i in imgstr.split()]
+    print(rowCount(imgStr2Int,nr,nc,thresholds))
+    print(columnCount(imgStr2Int,nr,nc,thresholds))
 
-my_function()
+#-------------------------------------------------
+
+def rowCount(threshList,nr,nc,thresholds):
+    rowCount = []
+    tmpCount = 0        
+    notif = 0
+    for i in range(nc*nr):
+        if threshList[i] > thresholds and notif == 0:
+            notif = 1
+            tmpCount = tmpCount + 1
+        elif threshList[i] <= thresholds:
+            notif = 0
+        if ((i+1)%nc == 0):
+            notif = 0
+            rowCount.append(tmpCount)
+            tmpCount = 0
+    return(rowCount)
+
+#-------------------------------------------------    
+
+def columnCount(imgStr2Int,nr,nc,thresholds):
+    rowCount = []
+    tmpCount = 0
+    notif = 0
+    for i in range(nc):
+        for n in range(nr):
+            if imgStr2Int[i+n*nc] > thresholds and notif == 0:
+                notif = 1
+                tmpCount = tmpCount + 1
+            elif imgStr2Int[i+n*nc] <= thresholds:
+                notif = 0
+            if ((n+1)%nr == 0):
+                notif = 0
+                rowCount.append(tmpCount)
+                tmpCount = 0
+    return(rowCount)
+
+#-------------------------------------------------
+
+def diagonalCount(imgStr2Int,nr,nc,thresholds):
+
+
+
+
+
+
+
+tda('imgFile',3,4,10)
+
+
+
+
+
+
+    
